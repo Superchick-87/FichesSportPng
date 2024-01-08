6b4c2c3e55b42f2907ebc5581e4675baece872ee
Superchick-87 <nicolas.peyrebrune@gmail.com>
2024-01-08 16:44:36 +0100

    maj access server

--- includes/accesserver.php
+++ includes/accesserver.php
@@ -1,32 +1,16 @@
 <?php
-	// server
-		// $database="info_prod";
-		// $serveur="localhost";
-		// $login="info_prod";
-		// $pass="vtN6wdvp4ZE7";
+// server
+// $redirectionPre="https://infographie.sudouest.fr/FichesSport/indexFPresentation.php?front=";
+// $redirectionCompR="https://infographie.sudouest.fr/FichesSport/indexFCompteRendu.php?front=";
+// $redirectionIndex="https://infographie.sudouest.fr/FichesSport/";
 
-		// $redirectionPre="https://infographie.sudouest.fr/FichesSport/indexFPresentation.php?front=";
-		// $redirectionCompR="https://infographie.sudouest.fr/FichesSport/indexFCompteRendu.php?front=";
-		// $redirectionIndex="https://infographie.sudouest.fr/FichesSport/";
+// local
+$redirectionPre = "http://localhost:8888/FichesSport_png/indexFPresentation.php?front=";
+$redirectionCompR = "http://localhost:8888/FichesSport_png/indexFCompteRendu.php?front=";
+$redirectionIndex = "http://localhost:8888/FichesSport_png/";
+$redirectionPre = "http://localhost:8888/FichesSport_png/indexFPresentation.php?front=";
 
-	// local
-		$database="Consultations";
-		$serveur="localhost";
-		$login="root";
-		$pass="root";
-		
-		$redirectionPre="http://localhost:8888/FichesSport_png/indexFPresentation.php?front=";
-		$redirectionCompR="http://localhost:8888/FichesSport_png/indexFCompteRendu.php?front=";
-		$redirectionIndex="http://localhost:8888/FichesSport_png/";
-		$redirectionPre="http://localhost:8888/FichesSport_png/indexFPresentation.php?front=";
-
-	// superchick !
-		// $database="superchicj955";
-		// $serveur="superchicj955.mysql.db";
-		// $login="superchicj955";
-		// $pass="g9KztZMkzRCN";
-		
-		// $redirectionPre="https://www.superchick.fr/FichesSport/indexFPresentation.php?front=";
-		// $redirectionCompR="https://www.superchick.fr/FichesSport/indexFCompteRendu.php?front=";
-		// $redirectionIndex="https://www.superchick.fr/FichesSport/";
-?>
+// superchick !
+// $redirectionPre="https://www.superchick.fr/FichesSport/indexFPresentation.php?front=";
+// $redirectionCompR="https://www.superchick.fr/FichesSport/indexFCompteRendu.php?front=";
+// $redirectionIndex="https://www.superchick.fr/FichesSport/";
